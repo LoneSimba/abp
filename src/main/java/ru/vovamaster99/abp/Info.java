38cc337fdfa4e3a33011c9296b40d2a856911281
VovaMaster <vovamaster99@gmail.com>
2015-09-22 12:45:01 +0300

    0.1 #2 HotFix
    I completely forget about models, but now they are there.

--- src/main/java/ru/vovamaster99/abp/Info.java
+++ src/main/java/ru/vovamaster99/abp/Info.java
@@ -3,7 +3,7 @@ package ru.vovamaster99.abp;
 public class Info {
 	
 	public static final String MODID = "abp";
-	public static final String VERSION = "2";
+	public static final String VERSION = "2hf";
 	public static final String NAME = "Apocalypse Build Pack";
 	public static final String CLIENT_PROXY = "ru.vovamaster99.abp.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "ru.vovamaster99.abp.proxy.CommonProxy";
